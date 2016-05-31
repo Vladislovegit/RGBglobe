a58aa4b13acb2412a5a125dc07bcf593a12f9142
vladislovegit <vladislovemail@gmail.com>
2016-06-01 00:48:58 +0300

    refactor

--- src/FileManager.java
+++ src/FileManager.java
@@ -1,23 +1,32 @@
 import javax.swing.*;
 import java.awt.*;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
 
 
-public class FileManager {
-    private static String FILENAME_RED = "./" + getTimeAndDate() + "_r.txt";
-    private static String FILENAME_GREEN = "./" + getTimeAndDate() + "_g.txt";
-    private static String FILENAME_BLUE = "./" + getTimeAndDate() + "_b.txt";
+class FileManager {
+    private String FILENAME_RED;
+    private String FILENAME_GREEN;
+    private String FILENAME_BLUE;
 
+    private String red;
+    private String green;
+    private String blue;
 
-    public static void saveFiles(JButton btns[][]) throws IOException {
-        String red = "{";
-        String green = "{";
-        String blue = "{";
+    FileManager() {
+        red = "{";
+        green = "{";
+        blue = "{";
 
+        FILENAME_RED = getFileName("r");
+        FILENAME_GREEN = getFileName("g");
+        FILENAME_BLUE = getFileName("b");
+
+        createFiles();
+    }
+
+    void saveFiles(JButton btns[][]) throws IOException {
         for (int i = 0, y = 0; i < RGBglobeUI.ROWS; i++, y += RGBglobeUI.SIZE_OF_BUTTON) {
-
             for (int j = 0, x = 0; j < RGBglobeUI.COLUMNS; j++, x += RGBglobeUI.SIZE_OF_BUTTON) {
                 String tail = ",";
                 if (i == RGBglobeUI.ROWS - 1 && j == RGBglobeUI.COLUMNS - 1) tail = "";
@@ -67,16 +76,19 @@ public class FileManager {
         red = red.concat("}");
         green = green.concat("}");
         blue = blue.concat("}");
-        createFiles();
         File.write(FILENAME_RED, red);
         File.write(FILENAME_GREEN, green);
         File.write(FILENAME_BLUE, blue);
     }
 
-    private static void createFiles() {
+    private void createFiles() {
         File.create(FILENAME_RED);
         File.create(FILENAME_GREEN);
         File.create(FILENAME_BLUE);
+    }
+
+    private String getFileName(String color) {
+        return "./" + getTimeAndDate() + "_" + color + ".txt";
     }
 
     private static String getTimeAndDate() {
