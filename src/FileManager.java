8ba588f6c1476aa455191847e9310a74b2f17fa7
vladislovegit <vladislovemail@gmail.com>
2016-06-01 00:38:25 +0300

    reworked algorithm of file saving

--- src/FileManager.java
+++ src/FileManager.java
@@ -10,12 +10,11 @@ public class FileManager {
     private static String FILENAME_GREEN = "./" + getTimeAndDate() + "_g.txt";
     private static String FILENAME_BLUE = "./" + getTimeAndDate() + "_b.txt";
 
-    public static void saveFiles(JButton btns[][]) throws IOException {
-        createFiles();
 
-        File.write(FILENAME_RED, "{");
-        File.write(FILENAME_GREEN, "{");
-        File.write(FILENAME_BLUE, "{");
+    public static void saveFiles(JButton btns[][]) throws IOException {
+        String red = "{";
+        String green = "{";
+        String blue = "{";
 
         for (int i = 0, y = 0; i < RGBglobeUI.ROWS; i++, y += RGBglobeUI.SIZE_OF_BUTTON) {
 
@@ -24,50 +23,54 @@ public class FileManager {
                 if (i == RGBglobeUI.ROWS - 1 && j == RGBglobeUI.COLUMNS - 1) tail = "";
 
                 if (btns[i][j].getBackground() == Color.RED) {
-                    File.update(FILENAME_RED, "1" + tail);
-                    File.update(FILENAME_GREEN, "0" + tail);
-                    File.update(FILENAME_BLUE, "0" + tail);
+                    red = red.concat("1" + tail);
+                    green = green.concat("0" + tail);
+                    blue = blue.concat("0" + tail);
                 }
                 if (btns[i][j].getBackground() == Color.GREEN) {
-                    File.update(FILENAME_RED, "0" + tail);
-                    File.update(FILENAME_GREEN, "1" + tail);
-                    File.update(FILENAME_BLUE, "0" + tail);
+                    red = red.concat("0" + tail);
+                    green = green.concat("1" + tail);
+                    blue = blue.concat("0" + tail);
                 }
                 if (btns[i][j].getBackground() == Color.BLUE) {
-                    File.update(FILENAME_RED, "0" + tail);
-                    File.update(FILENAME_GREEN, "0" + tail);
-                    File.update(FILENAME_BLUE, "1" + tail);
+                    red = red.concat("0" + tail);
+                    green = green.concat("0" + tail);
+                    blue = blue.concat("1" + tail);
                 }
                 if (btns[i][j].getBackground() == Color.CYAN) {
-                    File.update(FILENAME_RED, "0" + tail);
-                    File.update(FILENAME_GREEN, "1" + tail);
-                    File.update(FILENAME_BLUE, "1" + tail);
+                    red = red.concat("0" + tail);
+                    green = green.concat("1" + tail);
+                    blue = blue.concat("1" + tail);
                 }
                 if (btns[i][j].getBackground() == Color.YELLOW) {
-                    File.update(FILENAME_RED, "1" + tail);
-                    File.update(FILENAME_GREEN, "1" + tail);
-                    File.update(FILENAME_BLUE, "0" + tail);
+                    red = red.concat("1" + tail);
+                    green = green.concat("1" + tail);
+                    blue = blue.concat("0" + tail);
                 }
                 if (btns[i][j].getBackground() == Color.MAGENTA) {
-                    File.update(FILENAME_RED, "1" + tail);
-                    File.update(FILENAME_GREEN, "0" + tail);
-                    File.update(FILENAME_BLUE, "1" + tail);
+                    red = red.concat("1" + tail);
+                    green = green.concat("0" + tail);
+                    blue = blue.concat("1" + tail);
                 }
                 if (btns[i][j].getBackground() == Color.WHITE) {
-                    File.update(FILENAME_RED, "1" + tail);
-                    File.update(FILENAME_GREEN, "1" + tail);
-                    File.update(FILENAME_BLUE, "1" + tail);
+                    red = red.concat("1" + tail);
+                    green = green.concat("1" + tail);
+                    blue = blue.concat("1" + tail);
                 }
                 if (btns[i][j].getBackground() == Color.BLACK) {
-                    File.update(FILENAME_RED, "0" + tail);
-                    File.update(FILENAME_GREEN, "0" + tail);
-                    File.update(FILENAME_BLUE, "0" + tail);
+                    red = red.concat("0" + tail);
+                    green = green.concat("0" + tail);
+                    blue = blue.concat("0" + tail);
                 }
             }
         }
-        File.update(FILENAME_RED, "}");
-        File.update(FILENAME_GREEN, "}");
-        File.update(FILENAME_BLUE, "}");
+        red = red.concat("}");
+        green = green.concat("}");
+        blue = blue.concat("}");
+        createFiles();
+        File.write(FILENAME_RED, red);
+        File.write(FILENAME_GREEN, green);
+        File.write(FILENAME_BLUE, blue);
     }
 
     private static void createFiles() {
